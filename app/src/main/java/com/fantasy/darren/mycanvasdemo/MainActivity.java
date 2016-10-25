cb59b2bfd5ee2e73880e7d938087eb7ec78cbe64
Fantasy <782107743@qq.com>
2016-10-25 16:31:43 +0800

    实现画板功能

--- app/src/main/java/com/fantasy/darren/mycanvasdemo/MainActivity.java
+++ app/src/main/java/com/fantasy/darren/mycanvasdemo/MainActivity.java
@@ -1,13 +1,51 @@
 package com.fantasy.darren.mycanvasdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MyCanvas myCanvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myCanvas = (MyCanvas) findViewById(R.id.my_canvas);
+        Button saveBtn = (Button) findViewById(R.id.save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // savePhoto();
+            }
+        });
+    }
+
+    private void savePhoto(){
+        File file = new File(Environment.getExternalStorageDirectory(),System.currentTimeMillis()+".png");
+        try {
+            OutputStream outputStream = new FileOutputStream(file);
+            myCanvas.saveBitmap(outputStream);
+            outputStream.close();
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MEDIA_MOUNTED);
+            intent.setData(Uri.fromFile(Environment.getExternalStorageDirectory()));
+            sendBroadcast(intent);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/fantasy/darren/mycanvasdemo/MyCanvas.java
@@ -0,0 +1,79 @@
+package com.fantasy.darren.mycanvasdemo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.io.OutputStream;
+
+/**
+ * Created by fantasy on 16/10/25.
+ */
+public class MyCanvas extends View {
+    private static final String tag = "MyCanvas";
+    private Canvas mCanvas;
+    private Paint mPaint;
+    private Bitmap mBitmap;
+
+    public MyCanvas(Context context) {
+        super(context);
+    }
+
+    public MyCanvas(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mBitmap = Bitmap.createBitmap(2000, 2000, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+        mCanvas.drawColor(Color.GRAY);
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(10);
+    }
+
+    public MyCanvas(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private float startX, startY, stopX, stopY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i(tag, "ACTION_DOWN");
+                startX = event.getX();
+                startY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(tag, "ACTION_MOVE");
+                stopX = event.getX();
+                stopY = event.getY();
+                mCanvas.drawLine(startX, startY, stopX, stopY, mPaint);
+                startX = event.getX();
+                startY = event.getY();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mCanvas != null) {
+            Log.i(tag, "onDraw");
+            canvas.drawBitmap(mBitmap, 0, 0, mPaint);
+        }
+    }
+
+    public void saveBitmap(OutputStream outputStream) {
+        if (mBitmap != null)
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+    }
+}
