bfa842efb7eb5beb9236ff4e9b48ac223887383f
Fantasy <782107743@qq.com>
2016-12-08 16:49:36 +0800

    update

--- app/src/main/java/com/fantasy/darren/mycanvasdemo/widget/MyAnimationButton.java
+++ app/src/main/java/com/fantasy/darren/mycanvasdemo/widget/MyAnimationButton.java
@@ -5,31 +5,41 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.Button;
 
 /**
- * button中的字体可变色并且"一直播放"
+ * 具有动画效果的自定义button
  * Created by fantasy on 16/12/7.
  */
 
 public class MyAnimationButton extends Button {
-
+    // 背景画笔
+    private Paint mBackgroundPaint;
     private Paint mPaint;
     private String mText;
     private float mTextSize;
     private int mTextColor = Color.RED;
     private int mTextCoverColor = Color.GREEN;
     private int mProgress;
+    private RectF mBackgroundBounds;
+    // 背景颜色
+    private int mBackgroundColor = 0x00000000;
+    private int mBackgroundSecondColor =  Color.RED;
+    private int mBackgroundFirstColor = Color.GREEN;
+    private int mStockColor = 0xffbcbcbc;
     public MyAnimationButton(Context context) {
         super(context);
     }
 
     public MyAnimationButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setBackgroundColor(mBackgroundColor);
         mPaint = new Paint();
+        mBackgroundPaint = new Paint();
     }
 
     public MyAnimationButton(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -40,6 +50,7 @@ public class MyAnimationButton extends Button {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawText(canvas);
+        drawBackgroud(canvas);
     }
 
     private void drawText(Canvas canvas) {
@@ -67,6 +78,30 @@ public class MyAnimationButton extends Button {
         invalidate();
     }
 
+    private void drawBackgroud(Canvas canvas){
+        mBackgroundPaint.setColor(mBackgroundFirstColor);
+        mBackgroundBounds = new RectF();
+        float mProgressPercent = mProgress / (100 + 0f);
+        Log.i("darren","mProgressPercent:"+mProgressPercent);
+        LinearGradient mProgressBgGradient = new LinearGradient(0, 0, getMeasuredWidth(), 0, new int[] { mBackgroundFirstColor, mBackgroundSecondColor },
+                new float[] {mProgressPercent, mProgressPercent + 0.001f }, Shader.TileMode.CLAMP);
+        mBackgroundPaint.setColor(mBackgroundFirstColor);
+        mBackgroundPaint.setShader(mProgressBgGradient);
+        drawstock(canvas, mStockColor);
+        canvas.drawRoundRect(mBackgroundBounds, 5, 5, mBackgroundPaint);
+    }
+
+    private void drawstock(Canvas canvas, int color) {
+        // 画边框
+        Paint paint = new Paint();
+        paint.setColor(color);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(1);
+        paint.setAntiAlias(true);
+        RectF rectF = new RectF(0, 0, this.getWidth(), this.getHeight());
+        canvas.drawRoundRect(rectF, 5, 5, paint);
+
+    }
     public void setMyText(String text) {
         mText = text;
     }
