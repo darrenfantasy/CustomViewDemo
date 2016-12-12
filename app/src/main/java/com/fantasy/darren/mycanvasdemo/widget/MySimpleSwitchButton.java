61dc4205139ae1edaad92b8b3a6b9362d9570cf5
Fantasy <782107743@qq.com>
2016-12-12 17:38:23 +0800

    通过自定义属性来设置view所需要的资源

--- app/src/main/java/com/fantasy/darren/mycanvasdemo/widget/MySimpleSwitchButton.java
+++ app/src/main/java/com/fantasy/darren/mycanvasdemo/widget/MySimpleSwitchButton.java
@@ -7,8 +7,6 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.fantasy.darren.mycanvasdemo.R;
-
 /**
  * Created by fantasy on 16/12/12.
  * 仿iOS风格简单的开关
@@ -16,30 +14,41 @@ import com.fantasy.darren.mycanvasdemo.R;
 
 public class MySimpleSwitchButton extends View {
 
+    private String NAMESPACE = "http://schemas.android.com/apk/res/com.fantasy.darren.mycanvasdemo";
     private Bitmap mButtonOn;
     private Bitmap mButtonOff;
     private Bitmap mButtonCurrent;
     private boolean isOn;
+
     public MySimpleSwitchButton(Context context) {
         super(context);
+        initView(null);
     }
 
     public MySimpleSwitchButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initView();
+        initView(attrs);
     }
 
-    private void initView(){
-        mButtonOn = BitmapFactory.decodeResource(getResources(), R.mipmap.toggle_btn_checked);
-        mButtonOff = BitmapFactory.decodeResource(getResources(),R.mipmap.toggle_btn_unchecked);
+    private void initView(AttributeSet attrs) {
+        if (attrs != null) {
+            int mButtonOnId = attrs.getAttributeResourceValue(NAMESPACE, "switch_on", -1);
+            int mButtonOffId = attrs.getAttributeResourceValue(NAMESPACE, "switch_off", -1);
+            mButtonOn = BitmapFactory.decodeResource(getResources(), mButtonOnId);
+            mButtonOff = BitmapFactory.decodeResource(getResources(), mButtonOffId);
+            if (mButtonOn == null || mButtonOff == null) {
+                throw new NullPointerException("资源图片不能为空");
+            }
+        }
+
         setStatus(true);
         this.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isOn){
+                if (isOn) {
                     mButtonCurrent = mButtonOff;
                     isOn = false;
-                }else {
+                } else {
                     mButtonCurrent = mButtonOn;
                     isOn = true;
                 }
@@ -51,16 +60,16 @@ public class MySimpleSwitchButton extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(mButtonOn.getWidth(),mButtonOn.getHeight());
+        setMeasuredDimension(mButtonOn.getWidth(), mButtonOn.getHeight());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(mButtonCurrent,0,0,null);
+        canvas.drawBitmap(mButtonCurrent, 0, 0, null);
     }
 
-    public void setStatus(boolean b){
+    public void setStatus(boolean b) {
         isOn = b;
     }
 
